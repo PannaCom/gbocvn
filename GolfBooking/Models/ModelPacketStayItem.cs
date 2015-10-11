423ccb256f476e6a067d71b2c0cce137db9b1c3c
tuyenvv <tuyenvv@github.com>
2015-10-11 15:34:58 +0700

    Add missing file

---
+++ GolfBooking/Models/ModelPacketStayItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using GolfBooking.Models;
+
+namespace GolfBooking.Models
+{
+    public class ModelPacketStayItem:golf_package_stay
+    {
+        public IEnumerable<string> listImages { get; set; }
+    }
+}
---
+++ GolfBooking/Utility/Utility.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Text.RegularExpressions;
+namespace GolfBooking.Utility
+{
+    public class Utility
+    {
+
+        public static string stripTagHtml(string str)
+        {
+            return Regex.Replace(str, @"<[^>]*>", String.Empty);
+        }
+    }
+}
